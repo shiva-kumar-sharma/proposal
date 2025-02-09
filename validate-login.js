2147bce26dcfc3c19296fc1773912fc85e44cc5e
shiva-kumar-sharma <shivafreek842@gmail.com>
2025-02-09 22:02:41 +0530

    Add files via upload

--- validate-login.js
+++ validate-login.js
@@ -11,3 +11,14 @@ if (!sessionStorage.getItem('username')) {
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault(); // Disable right-click
 });
+
+document.addEventListener("keydown", function (event) {
+    if (
+        event.keyCode === 123 || // F12
+        (event.ctrlKey && event.shiftKey && event.keyCode === 73) || // Ctrl+Shift+I
+        (event.ctrlKey && event.shiftKey && event.keyCode === 74) || // Ctrl+Shift+J
+        (event.ctrlKey && event.keyCode === 85) // Ctrl+U
+    ) {
+        event.preventDefault();
+    }
+});
